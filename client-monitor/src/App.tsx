cc2fb015206439f63768c04ee4a1391be7954b84
Zachary Sussman <zachary@zacharysussman.com>
2019-04-06 17:22:31 -0400

    added website and idle animations

--- client-monitor/src/App.tsx
+++ client-monitor/src/App.tsx
@@ -2,11 +2,25 @@ import React, { Component } from 'react';
 import Square from './Square';
 import TileSprite from './Tile';
 import './App.css';
-import { GameState, Tile, Color, TileType } from './shared/GameTypes';
+import { GameState, Tile, Color, TileType, GamePhase } from './shared/GameTypes';
+
+import Canvas from './images/canvas.png';
+import UCStones from './images/ucstones.png';
+import StackOverflow from './images/stackoverflow.png';
+import Wikipedia from './images/wikipedia.png';
+import YouTube from './images/youtube.png';
 
 import Socket from 'socket.io-client';
 
 var socket: SocketIOClient.Socket = Socket('http://localhost:3000/');
+
+let images = [
+  Canvas,
+  UCStones,
+  StackOverflow,
+  Wikipedia,
+  YouTube
+];
 
 type GameStateCallback = ((gs: GameState) => void);
 
@@ -14,33 +28,44 @@ function subscribeToSocket(gameStateCallback: GameStateCallback) {
   socket.on('connect', () => {
     socket.emit('identification', 'console');
   });
-  socket.on('update-game-state', (gs: GameState) => {
+  socket.on('game-state-updated', (gs: GameState) => {
     gameStateCallback(gs);
   });
 }
 
-type State = { squareMap: boolean[][] | null, tiles: Tile[] | null, solves: number };
+type State = { playing: boolean, squareMap: boolean[][] | null, tiles: Tile[] | null, solves: number, time: number };
 
 class App extends Component<{}, State> {
   constructor(props: {}) {
     super(props);
-    this.state = { squareMap: null, tiles: [{ color: Color.Red, type: TileType.T }, { color: Color.Green, type: TileType.Z }], solves: 0 };
+    this.state = { playing: false, squareMap: null, tiles: [{ color: Color.Red, type: TileType.T }, { color: Color.Green, type: TileType.Z }], solves: 0, time: 25 };
+    subscribeToSocket(this.updateGameState.bind(this));
   }
 
   updateGameState(gameState: GameState) {
-    if (gameState && gameState.puzzles && gameState.puzzles[0]) {
-      this.setState({
-        squareMap: gameState.puzzles[0].grid.map(row => row.map(x => x === 1)),
-        tiles: gameState.puzzles[0].ingredients,
-        solves: gameState.solves[0]
-      });
-    } else {
-      this.setState({ squareMap: null, tiles: null, solves: gameState.solves[0] });
-    }
+    let in_puzzle = gameState && gameState.puzzles && gameState.puzzles[0];
+    this.setState({
+      playing: gameState.phase === GamePhase.Playing,
+      squareMap: in_puzzle ?
+        gameState.puzzles[0].grid.map(row => row.map(x => x === 1)) : null,
+      tiles: in_puzzle ? gameState.puzzles[0].ingredients : null,
+      solves: gameState.solves[0],
+      time: gameState.time
+    });
   }
 
   render() {
-    let { squareMap, tiles, solves } = this.state;
+    return this.state.playing ? this.renderPlaying() : this.renderIdle();
+  }
+
+  renderIdle() {
+    return <div className="idlebox">
+      Please wait for game to start
+    </div>
+  }
+
+  renderPlaying() {
+    let { squareMap, tiles, solves, time } = this.state;
     let squares = [0, 1, 2, 3].map(row => [0, 1, 2, 3].map(col => (
       <Square
         key={"" + row + col}
@@ -60,7 +85,9 @@ class App extends Component<{}, State> {
     return (
       <div className="App">
         <div className="graphics">
-          <div className="website" />
+          <div className="website">
+            <img className="website-image" src={Wikipedia} />
+          </div>
           {squares}
           <div className="tiles-container">
             {tile_sprites}
@@ -68,6 +95,9 @@ class App extends Component<{}, State> {
         </div>
         <div className="solvebox">
           Solved: {solves}
+        </div>
+        <div className="timebox">
+          Time: {time}
         </div>
       </div>
     );
